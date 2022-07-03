3fff9769870724210e7d4fad6e796a397686e2c2
mreshtaiwi <mr.eshtaiwi@gmail.com>
2022-07-03 17:44:53 +0300

    testing the actions aws

---
+++ server.js
@@ -0,0 +1,13 @@
+"use strict";
+
+const express = require("express");
+const PORT = process.env.PORT || 8080;
+const app = new express();
+
+app.get("/", (req, res) => {
+    res.status(200).send("pizza time");
+});
+
+app.listen(PORT, () => {
+    console.log(`server is working on ${PORT}`);
+});
